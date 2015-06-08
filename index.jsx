2f617391f301bcd999eb0876f882583621094541
kunalrai <ikunalrai@gmail.com>
2015-06-09 00:56:24 +0530

    configure webpack dev server

--- index.jsx
+++ index.jsx
@@ -1,7 +1,9 @@
-
+var React = require('react');
 
 var App = React.createClass({
     render:function(){
         return (<div>Hello World</div>);
     }
 });
+
+React.render(<App/>,document.getElementById('content'));
