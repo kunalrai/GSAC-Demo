3485cca1ef2b5bb97d63c0c53040b4d656c394a8
ikunalrai@gmail.com <ikunalrai@gmail.com>
2015-06-09 11:45:19 +0530

    gsap react plugin

--- index.jsx
+++ index.jsx
@@ -1,8 +1,23 @@
 var React = require('react');
 
+
 var App = React.createClass({
+    getInitialState:function(){
+
+        return {width:0,height:0};
+    },
+
+    componentDidMount:function(){
+        TweenLite.to(this,2,{state:{width:100, height:100,backgroundColor:"Red"}, ease:Power4.easeInOut});
+    },
+
     render:function(){
-        return (<div>Hello World</div>);
+        return (<div style={{width:this.state.width,
+                             height :this.state.height,
+                             backgroundColor:this.state.backgroundColor
+
+                             }}>
+            </div>);
     }
 });
 
